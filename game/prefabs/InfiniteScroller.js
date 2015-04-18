f0c0c80be93fd2d9031dac0d799417746871eff9
Ben McEldowney <bmceldowney@weareratio.com>
2015-04-18 12:08:42 -0700

    adding parallaxStage to manage backgrounds

--- game/prefabs/InfiniteScroller.js
+++ game/prefabs/InfiniteScroller.js
@@ -1,23 +1,41 @@
 'use strict';
 
-var InfiniteScroller = function(game, x, y, frame, textureName, speed) {
+var InfiniteScroller = function(game, x, y, textureName, speed) {
+	Phaser.Sprite.call(this, game, 0, 0)
+	this.game = game;
   this.sprite = this.game.add.sprite(0, 0, textureName);
-  this.spriteAlt = this.game.add.sprite(sprite.width, 0, textureName);
+  this.altSprite = this.game.add.sprite(this.sprite.width, 0, textureName);
+  this.currentSprite = this.sprite;
+  this.stagedSprite = this.altSprite;
 
-  
+  this.sprite.smoothed = false;
+  this.altSprite.smoothed = false;
+
+  this.sprite.scale.setTo(4, 4);
+  this.altSprite.scale.setTo(4, 4);
+
   this.scrollSpeed = speed;
-  
 };
 
 InfiniteScroller.prototype = Object.create(Phaser.Sprite.prototype);
 InfiniteScroller.prototype.constructor = InfiniteScroller;
 
 InfiniteScroller.prototype.update = function() {
-	this.x -= this.scrollSpeed;
+	this.sprite.x -= this.scrollSpeed;
+	this.altSprite.x -= this.scrollSpeed;
 
-	if ((this.x + this.width) > game.width) {
+	checkForSwap.call(this, this.sprite, this.altSprite);
 
-	};  
 };
+
+function checkForSwap(sprite, altSprite) {
+	if ((this.currentSprite.width + this.currentSprite.x) < this.game.width) {
+		var stagedSprite = this.stagedSprite;
+		this.stagedSprite.x = this.game.width - 1;
+
+		this.stagedSprite = this.currentSprite;
+		this.currentSprite = stagedSprite;
+	};  	  
+}
 
 module.exports = InfiniteScroller;
