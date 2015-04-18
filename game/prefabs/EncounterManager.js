a6274dd4fb7be510aba38e6ef126cf0263a2f369
Ben McEldowney <bmceldowney@weareratio.com>
2015-04-18 16:04:48 -0700

    adding encounter manager

---
+++ game/prefabs/EncounterManager.js
@@ -0,0 +1,12 @@
+'use strict';
+var EncounterManager = function(game) {
+	this.game = game;
+};
+
+EncounterManager.prototype.constructor = EncounterManager;
+
+EncounterManager.prototype.start = function() {
+
+};
+
+module.exports = EncounterManager;
