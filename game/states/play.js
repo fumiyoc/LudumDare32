3a474994ad06d7e6a76fa5eae71d56b820c49299
Ben McEldowney <bmceldowney@weareratio.com>
2015-04-18 12:08:42 -0700

    adding parallaxStage to manage backgrounds

--- game/states/play.js
+++ game/states/play.js
@@ -1,40 +1,28 @@
+var ParallaxStage = require('../prefabs/ParallaxStage');
 
-  'use strict';
-  function Play() {}
-  Play.prototype = {
-    create: function() {
-      this.game.physics.startSystem(Phaser.Physics.ARCADE);
+'use strict';
+function Play() {}
+Play.prototype = {
+  create: function() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-      this.forestBack = this.game.add.sprite(0, 0, 'forestBack');
-      this.forestLights = this.game.add.sprite(0, 0, 'forestLights');
-      this.forestMiddle = this.game.add.sprite(0, 0, 'forestMiddle');
-      this.forestFront = this.game.add.sprite(0, 0, 'forestFront');
-      this.forestBack2 = this.game.add.sprite(this.forestFront., 0, 'forestBack');
-      this.forestLights2 = this.game.add.sprite(0, 0, 'forestLights');
-      this.forestMiddle2 = this.game.add.sprite(0, 0, 'forestMiddle');
-      this.forestFront2 = this.game.add.sprite(0, 0, 'forestFront');
-      
-      this.forestBack.scale.setTo(4, 4);
-      this.forestLights.scale.setTo(4, 4);
-      this.forestMiddle.scale.setTo(4, 4);
-      this.forestFront.scale.setTo(4, 4);
+    var parallaxStageConfig = [
+      { imageName: 'forestBack', speed: 0.5 },
+      { imageName: 'forestLights', speed: 0.75 },
+      { imageName: 'forestMiddle', speed: 1 },
+      { imageName: 'forestFront', speed: 1.25 }
+    ];
 
-      this.forestBack.smoothed = false;
-      this.forestLights.smoothed = false;
-      this.forestMiddle.smoothed = false;
-      this.forestFront.smoothed = false;
+    this.parallaxStage = new ParallaxStage(this.game, parallaxStageConfig);
+  },
 
+  update: function() {
+    this.parallaxStage.update();
+  },
 
-    },
-    update: function() {
-      this.forestBack.x -= 0.5;
-      this.forestLights.x -= 0.75;
-      this.forestMiddle.x -= 1;
-      this.forestFront.x -= 1.25;
-    },
-    clickListener: function() {
-      this.game.state.start('gameover');
-    }
-  };
-  
-  module.exports = Play;
+  clickListener: function() {
+    this.game.state.start('gameover');
+  }
+};
+
+module.exports = Play;
