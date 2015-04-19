21787f34b7cb2f57013dda1ebe64744879470407
ninjascribble <scott@ninjascript.com>
2015-04-18 17:58:37 -0700

    Items, sort of

--- game/prefabs/menu/index.js
+++ game/prefabs/menu/index.js
@@ -1,5 +1,6 @@
 var Panel = require('./../panel');
 var CommandList = require('./commandList');
+var ItemList = require('./itemList');
 
 function Menu(game, x, y, w, h) {
 
@@ -9,14 +10,14 @@ function Menu(game, x, y, w, h) {
 
   this.panel = new Panel(game, 0, 0, w, h);
   this.commands = new CommandList(game, this);
+  this.items = new ItemList(game, this);
 
   this.addChild(this.panel);
   this.addChild(this.commands);
+  this.addChild(this.items);
 }
 
 Menu.prototype = Object.create(Phaser.Sprite.prototype);
 Menu.constructor = Menu;
-
-
 
 module.exports = Menu;
